5c162d5c5ef6e139e1acc5939e4bfdcf55efa8cc
daniil.sosnovsky <dsosnovsky@aquivalabs.com>
2024-07-12 19:57:05 -0300

    Added Configuration.remote

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -28,6 +28,7 @@ public class BaseTest {
         RestAssured.baseURI = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 10000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("incognito");
